0d4a0e23cd3909214225bb531943a35f1beb0c39
Jonas Claes <jonas@jonasclaes.be>
2021-12-15 13:58:26 +0100

    Empty!

--- src/util/Server.ts
+++
@@ -1,5 +0,0 @@
-export default class Server {
-    public static endpoint = "https://appwrite.jonasclaes.be/v1";
-    public static projectId = "61ab3f7d9d4d8";
-    public static projectsCollectionId = "61ab6582e11c8";
-}
